9759bf5724859a5bbbbe2b28dc7b8d6822e7c699
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-10-27 20:09:13 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
-plugins {
-    id("java")
-}
-
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 dependencies {
