3283bd08fb224e860abbf00d827d7752abac7819
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-10-29 19:18:47 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,31 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                0 - Exit""");
+
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+        System.out.println();
+
+        switch (choice) {
+            case 1:
+                Cli.greeting();
+            case 2:
+                Even.gameEven();
+            case 0:
+                break;
+        }
+
+        scanner.close();
+
     }
 }
-
