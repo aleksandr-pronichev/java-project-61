f09515b18520ab8a0d735769fc2cda806cb4d6d1
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-10-29 21:32:09 +0300

    fix Even.java

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,16 +1,13 @@
 package hexlet.code;
 
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Even {
     public static void gameEven() {
-        System.out.println("Welcome to the Brain Games!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
+        String userName = Engine.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < 3; i++) {
             int guessedNumber = (int) (Math.random() * 100);
@@ -19,23 +16,26 @@ public class Even {
             String answer = scanner.next();
 
             if (!answer.equals("yes") && !answer.equals("no")) {
-                System.out.println("'" + answer + "'" + "is wrong answer ;(. Correct answer was 'yes'.\n" +
-                        "Let's try again, " + userName + "!");
+                System.out.println("'" + answer + "'" + "is wrong answer. Answer should be 'yes' or 'no'");
                 break;
             }
-            if (guessedNumber % 2 == 0 && answer.equals("no")) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\n" +
-                        "Let's try again, " + userName + "!");
-                break;
-            } else if (guessedNumber % 2 == 0 && answer.equals("yes")) {
-                System.out.println("Correct!");
-            }
-            if (guessedNumber % 2 != 0 && answer.equals("yes")) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n" +
-                        "Let's try again, " + userName + "!");
-                break;
-            } else if (guessedNumber % 2 != 0 && answer.equals("no")){
-                System.out.println("Correct!");
+
+            if (guessedNumber % 2 == 0) {
+                if (answer.equals("yes")) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\n"
+                            + "Let's try again, " + userName + "!");
+                    break;
+                }
+            } else {
+                if (answer.equals("no")) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n"
+                            + "Let's try again, " + userName + "!");
+                    break;
+                }
             }
 
             if (i == 2) {
