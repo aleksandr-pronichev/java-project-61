e49a135edb0f2b558cd49c0b116475b5f63d0390
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-10-29 21:48:05 +0300

    add Engine.java for common logic for games

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static String greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        return userName;
+    }
+}
