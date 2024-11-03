5f0b751b47032ee61622a36b58e903048e2869e7
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-11-03 20:30:26 +0300

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,6 +3,8 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
+    public static final int ROUNDS = 3;
+
     public static String greeting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
@@ -10,5 +12,28 @@ public class Engine {
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
         return userName;
+    }
+
+    public static void game(String rules, String[] questions, String[] correctAnswers) {
+        String userName = greeting();
+        System.out.println(rules);
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < Engine.ROUNDS; i++) {
+            System.out.println("Question: " + questions[i]);
+            System.out.print("Your answer: ");
+            String answer = scanner.next();
+
+            if (answer.equals(correctAnswers[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. "
+                        + "Correct answer was '" + correctAnswers[i] + "'.");
+                System.out.println("Let`s try again, " + userName + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
