192ef07efd7f7ae3fdd9e1e17b6261b09505e4a2
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-11-03 20:30:48 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,50 +1,24 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
 import hexlet.code.Engine;
 
 public class Even {
+
+    private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
     public static void gameEven() {
-        String userName = Engine.greeting();
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        String[] questions = new String[Engine.ROUNDS];
+        String[] correctAnswers = new String[Engine.ROUNDS];
 
-        Scanner scanner = new Scanner(System.in);
-
-        for (int i = 0; i < 3; i++) {
-            int guessedNumber = (int) (Math.random() * 100);
-            System.out.println("Question: " + guessedNumber);
-            System.out.print("Your answer: ");
-            String answer = scanner.next();
-
-            if (!answer.equals("yes") && !answer.equals("no")) {
-                System.out.println("'" + answer + "'" + "is wrong answer. Answer should be 'yes' or 'no'");
-                break;
-            }
-
-            if (guessedNumber % 2 == 0) {
-                if (answer.equals("yes")) {
-                    System.out.println("Correct!");
-                } else {
-                    System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\n"
-                            + "Let's try again, " + userName + "!");
-                    break;
-                }
+        for (int i = 0; i < Engine.ROUNDS; i++) {
+            int number = (int) (Math.random() * 100);
+            questions[i] = Integer.toString(number);
+            if (number % 2 == 0) {
+                correctAnswers[i] = "yes";
             } else {
-                if (answer.equals("no")) {
-                    System.out.println("Correct!");
-                } else {
-                    System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n"
-                            + "Let's try again, " + userName + "!");
-                    break;
-                }
-            }
-
-            if (i == 2) {
-                System.out.println("Congratulations, " + userName + "!");
-                break;
+                correctAnswers[i] = "no";
             }
         }
-        scanner.close();
+        Engine.game(RULES, questions, correctAnswers);
     }
-
 }
