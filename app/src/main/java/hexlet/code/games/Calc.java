4c931ed4f0cdae5ad74a1f491f4428333af55f39
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-11-03 20:31:01 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,8 +3,34 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Calc {
-    public static void gameCalc() {
-        String userName = Engine.greeting();
 
+    private static final String RULES = "What is the result of the expression?";
+
+    public static void gameCalc() {
+        String[] questions = new String[Engine.ROUNDS];
+        String[] correctAnswers = new String[Engine.ROUNDS];
+        String[] operators = {"+", "-", "*"};
+
+        for (int i = 0; i < Engine.ROUNDS; i++) {
+            int firstNumber = (int) (Math.random() * 100);
+            int secondNumber = (int) (Math.random() * 100);
+            String operator = operators[(int) (Math.random() * operators.length)];
+            questions[i] = firstNumber + " " + operator + " " + secondNumber;
+
+            switch (operator) {
+                case "+":
+                    correctAnswers[i] = Integer.toString(firstNumber + secondNumber);
+                    break;
+                case "-":
+                    correctAnswers[i] = Integer.toString(firstNumber - secondNumber);
+                    break;
+                case "*":
+                    correctAnswers[i] = Integer.toString(firstNumber * secondNumber);
+                    break;
+                default:
+                    break;
+            }
+        }
+        Engine.game(RULES, questions, correctAnswers);
     }
 }
