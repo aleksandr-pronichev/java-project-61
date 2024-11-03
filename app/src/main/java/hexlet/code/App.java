cf364b247798ad1ef59d62b804667c911b4f18d6
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-11-03 20:30:06 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,9 @@
 package hexlet.code;
 
 import java.util.Scanner;
+
 import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +13,7 @@ public class App {
                 Please enter the game number and press Enter.
                 1 - Greet
                 2 - Even
+                3 - Calc
                 0 - Exit""");
 
         System.out.print("Your choice: ");
@@ -20,15 +23,18 @@ public class App {
         switch (choice) {
             case 1:
                 Cli.greeting();
+                break;
             case 2:
                 Even.gameEven();
+                break;
+            case 3:
+                Calc.gameCalc();
+                break;
             case 0:
                 break;
             default:
                 System.out.println("Please select 1, 2, 3, 4, 5, 6 or 0.");
         }
-
         scanner.close();
-
     }
 }
