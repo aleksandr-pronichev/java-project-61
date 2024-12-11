8839eb2bf958ec2c2d480fb32c8ee7872edfaff4
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2024-12-11 19:38:31 +0300

    add Progression game

---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,20 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class Progression {
+
+    private static final String RULES = "What number is missing in the progression?";
+
+    public static void gameProgression() {
+
+
+    }
+
+    public static void generateProgression(int firstNumber, int step, int length) {
+        int currentNumber = firstNumber;
+        for (int i = 0; i < length; i++) {
+            
+        }
+    }
+}
