c0609b8465e568ff137ac80f98b6897281346fbf
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-01-10 22:36:34 +0300

    fix

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -30,13 +30,4 @@ public class Engine {
         }
         System.out.println("Congratulations, " + userName + "!");
     }
-
-    public static String greeting() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-        return userName;
-    }
 }
